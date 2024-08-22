95b9ba591bce03e5eb3fdff0e2898fba493aea0f
Sameer Kumar <kumar.sam1612@gmail.com>
2024-08-22 22:19:01 +0530

    added date-fns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import {
+  differenceInMilliseconds,
+  differenceInSeconds,
+  Duration,
+  intervalToDuration,
+} from "date-fns";
 import { useEffect, useState } from "react";
 
 const dates = {
@@ -7,7 +13,7 @@ const dates = {
 };
 
 export default function Home() {
-  const [duration, setDuration] = useState({
+  const [duration, setDuration] = useState<Duration>({
     years: 0,
     months: 0,
     days: 0,
@@ -37,14 +43,14 @@ export default function Home() {
         <h2 className="text-2xl font-semibold">Engaged since</h2>
         <div className="justify-center text-sm opacity-50 flex flex-col gap-2">
           <div className="justify-center flex gap-2">
-            <span>{duration.years} years</span>
-            <span>{duration.months} months</span>
-            <span>{duration.days} days</span>
+            <span>{duration.years ?? 0} years</span>
+            <span>{duration.months ?? 0} months</span>
+            <span>{duration.days ?? 0} days</span>
           </div>
           <div className="justify-center flex gap-2">
-            <span>{duration.hours} hrs</span>
-            <span>{duration.minutes} mins</span>
-            <span>{duration.seconds} secs</span>
+            <span>{duration.hours ?? 0} hrs</span>
+            <span>{duration.minutes ?? 0} mins</span>
+            <span>{duration.seconds ?? 0} secs</span>
           </div>
         </div>
       </div>
@@ -56,41 +62,10 @@ function timeUntil(targetDate: string) {
   const now = new Date();
   const target = new Date(targetDate);
 
-  let years = Math.abs(target.getFullYear() - now.getFullYear());
-  let months = Math.abs(target.getMonth() - now.getMonth());
-  let days = Math.abs(target.getDate() - now.getDate());
-  let hours = Math.abs(target.getHours() - now.getHours());
-  let minutes = Math.abs(target.getMinutes() - now.getMinutes());
-  let seconds = Math.abs(target.getSeconds() - now.getSeconds());
+  const diff = Math.abs(differenceInMilliseconds(now, target));
 
-  if (seconds < 0) {
-    seconds += 60;
-    minutes--;
-  }
-  if (minutes < 0) {
-    minutes += 60;
-    hours--;
-  }
-  if (hours < 0) {
-    hours += 24;
-    days--;
-  }
-  if (days < 0) {
-    const lastMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-    days += lastMonth.getDate();
-    months--;
-  }
-  if (months < 0) {
-    months += 12;
-    years--;
-  }
-
-  return {
-    years: years,
-    months: months,
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds,
-  };
+  return intervalToDuration({
+    start: 0,
+    end: diff,
+  });
 }
