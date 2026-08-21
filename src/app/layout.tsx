8eaadbf2cbdac8183bd92b05ed2c40d5c2100a99
Sameer Kumar <kumar.sam1612@gmail.com>
2026-08-21 09:57:48 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jyoti-Sameer",
-  description: "Created with love.",
+  description: "Waiting with love.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
+  openGraph: {
+    title: "Jyoti-Sameer",
+    description: "Waiting with love.",
+    images: ["/logo.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jyoti-Sameer",
+    description: "Waiting with love.",
+    images: ["/logo.png"],
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
